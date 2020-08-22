2c8af957f3f53fb5c4fe458cc9905021aab41999
hansin91 <hansinsusatya@gmail.com>
2020-08-22 16:27:05 +0700

    Migration File Create Uploaded Order

---
+++ migrations/1598080290311-Create UploadedOrdetTable.ts
@@ -0,0 +1,64 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateUploadedOrdetTable1598080290311 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+      await queryRunner.createTable(new Table({
+        name: 'm_uploaded_order',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isGenerated: true,
+            isPrimary: true,
+            isNullable: false,
+            generationStrategy: 'increment'
+          },
+          {
+            name: 'name',
+            type: 'text',
+            isNullable: false
+          },
+          {
+            name: 'file_path',
+            type: 'text',
+            isNullable: false
+          },
+          {
+            name: 'store_id',
+            type: 'int',
+            isNullable: false
+          },
+          {
+            name: 'created_date',
+            type: 'datetime',
+            isNullable: false,
+            default: 'CURRENT_TIMESTAMP'
+          },
+          {
+            name: 'finished_date',
+            type :'datetime',
+            isNullable: true,
+            default: 'CURRENT_TIMESTAMP'
+          },
+          {
+            name: 'is_processing',
+            type: 'boolean',
+            isNullable: false,
+            default: 0
+          },
+          {
+            name: 'created_user',
+            type: 'int',
+            isNullable: false,
+            default: 1
+          },
+        ]
+      }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+      await queryRunner.dropTable('m_uploaded_order');
+    }
+
+}
