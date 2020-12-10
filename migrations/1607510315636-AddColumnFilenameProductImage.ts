40ddb59079f074779013b40b27b40daa0e0d0907
hansin91 <hansinsusatya@gmail.com>
2020-12-10 17:13:59 +0700

    Edit migration file

--- migrations/1607510315636-AddColumnFilenameProductImage.ts
+++ migrations/1607510315636-AddColumnFilenameProductImage.ts
@@ -4,13 +4,6 @@ export class AddColumnFilenameProductImage1607510315636 implements MigrationInte
 
     public async up(queryRunner: QueryRunner): Promise<any> {
       await queryRunner.addColumn('m_product_image', new TableColumn({
-        name: 'filename',
-        type: 'varchar',
-        isNullable: true,
-        isUnique: true
-      }))
-
-      await queryRunner.addColumn('m_product_image', new TableColumn({
         name: 'extension',
         type: 'varchar',
         isNullable: true
@@ -18,7 +11,6 @@ export class AddColumnFilenameProductImage1607510315636 implements MigrationInte
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
-      await queryRunner.dropColumn('m_product_image', 'filename')
       await queryRunner.dropColumn('m_product_image', 'extension')
     }
 }
